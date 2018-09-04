11d1d4302c0a1df634603695c9202523b72035b8
Oscar González <osdagoso@hotmail.com>
2018-09-04 12:05:46 -0500

    Added dummy user data to DB schema script

--- flask-api/data/database_script.sql
+++ flask-api/data/database_script.sql
@@ -3,8 +3,13 @@ CREATE TABLE Users (
     fname VARCHAR(32) NOT NULL,
     lname VARCHAR(32) NOT NULL,
 	password VARCHAR(255) NOT NULL,
-	country INT,
     email VARCHAR(64) UNIQUE NOT NULL,
+	usertype INT NOT NULL,
+	country INT,
 	iduva VARCHAR(64) UNIQUE,
 	idicpc VARCHAR(64) UNIQUE
 );
+
+INSERT INTO Users
+VALUES ("admin","Admin","Admin","admin","admin@admin.com",0,NULL,NULL,NULL),
+	   ("osdagoso","Oscar","Gonzalez","osgo2030","osdagoso@hotmail.com",1,NULL,NULL,NULL);
