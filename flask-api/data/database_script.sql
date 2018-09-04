36ecaf7cbc82fd1c047711084383ea0a95a86b5a
Oscar González <osdagoso@hotmail.com>
2018-09-04 11:43:33 -0500

    Added initial DB schema for Users table

---
+++ flask-api/data/database_script.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Users (
+	username VARCHAR(64) NOT NULL PRIMARY KEY,
+    fname VARCHAR(32) NOT NULL,
+    lname VARCHAR(32) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	country INT,
+    email VARCHAR(64) UNIQUE NOT NULL,
+	iduva VARCHAR(64) UNIQUE,
+	idicpc VARCHAR(64) UNIQUE
+);
