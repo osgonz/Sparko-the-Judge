a48ab473bff46b8ce3d2970250c6d5c8b594f287
Jessica Cavazos <jessycavazos95@gmail.com>
2018-09-05 23:37:05 -0500

    Login View
    Front-end React:
    - Created Login Component
    - Added axios for http requests
    
    Back-end Flask:
    - Created Authentication class to handle user login
    - Created Create User class
    
    Database:
    - Created Stored Procedures to handle login and
    creating new user.
    
    Notes:
    - Install axios on react with: npm install axios
    - Run the Procedures.sql in mysql, if there is an
    error try doing copy paste instead of running file

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import axios from 'axios'
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            userId: '',
+            username: '',
+            password: ''
+        }
+        this.handleLogin = this.handleLogin.bind(this)
+    }
+
+    handleLogin () {
+        const { username, password } = this.state;
+        axios.post('http://127.0.0.1:5000/AuthenticateUser',{
+            username: username,
+            password: password
+        })
+        .then(response => this.setState({userId: response.data.UserId}))
+        .then(response => console.log(this.state.userId))
+        .catch((error) => {
+              console.log(error);
+            });
+    }
+
+    loginChange (user) {
+        this.setState({username: user})
+    }
+
+    passwordChange (password) {
+        this.setState({password: password})
+    }
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <center>
+            <Card style={{raised: true, width: '25%'}} >
+                <CardHeader
+                  style={{titleColor: "inherit", color: "inherit"}}
+                  title="Login"
+                />
+                <CardContent>
+                           <TextField
+                             id="login"
+                             label="Username"
+                             margin="none"
+                             style = {{width: '90%'}}
+                             onChange={this.loginChange.bind(this)}
+                           />
+                           <br/>
+                           <TextField
+                             id="password"
+                             type="password"
+                             label="Password"
+                             margin="none"
+                             style = {{width: '90%'}}
+                             onChange={this.passwordChange.bind(this)}
+                           />
+                           <br/>
+                           <br/>
+                           <Button
+                             variant="contained"
+                             margin="normal"
+                             color="primary"
+                             type="submit"
+                             style= {{width: '30%', backgroundColor: "#0F2027", titleColor: "#FFFFFF"}}
+                             onClick={this.handleLogin.bind()}
+                           >
+                              Login
+                           </Button>
+                           <br/>
+                           <br/>
+                           <Button
+                             variant="contained"
+                             margin="normal"
+                             color="primary"
+                             type="submit"
+                             style= {{width: '30%', backgroundColor: "#0F2027", titleColor: "#FFFFFF"}}
+                           >
+                              Create Account
+                           </Button>
+                </CardContent>
+            </Card>
+            </center>
+        );
+      }
+}
+
+export default Login;
