4b4751eee43418ca87caa778cfb26d6fc61338f9
Oscar González <osdagoso@hotmail.com>
2018-09-07 13:15:56 -0500

    Replaced navbar text buttons for icon buttons, condensed user options into a dropdown menu

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import IconButton from "@material-ui/core/IconButton/IconButton";
+import Menu from "@material-ui/core/Menu/Menu";
+import MenuItem from "@material-ui/core/MenuItem/MenuItem";
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+
+import Person from '@material-ui/icons/Person';
+import FitnessCenter from '@material-ui/icons/FitnessCenter';
+import Group from '@material-ui/icons/Group';
 
 import '../../style/style.css';
 
@@ -22,41 +29,44 @@ const styles = {
 // Header component embodies the site's navigation bar
 // Props: isLogged (boolean) and isAdmin (boolean)
 class Header extends Component {
+    state = {
+        anchor: null,
+    };
+
+    handleMenuToggle = event => {
+        this.setState({
+            anchor: event.currentTarget
+        });
+    };
+
+    handleMenuToggleClose = () => {
+        this.setState({
+            anchor: null
+        });
+    };
+
     render() {
         const { classes } = this.props;
 
-        // If logged in, show the pertinent buttons for admins and regular users
-        // If not logged in, just show a 'log in' button
-        const headerButtons = this.props.isLogged ? (
-            <div className="header-bar">
-                <Link to='/contests' style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <Button className="header-button">
-                        Contests
-                    </Button>
-                </Link>
-                {this.props.isAdmin &&
-                    <Link to='/users' style={{textDecoration: 'none', color: 'inherit'}}>
-                        <Button className="header-button">
-                            Users
-                        </Button>
-                    </Link>
-                }
-                <Link to='/profile' style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <Button className="header-button">
-                        Profile
-                    </Button>
-                </Link>
-                <Button className="header-button">
-                    Log Out
-                </Button>
+        const isMenuOpen = Boolean(this.state.anchor);
+
+        // If logged in, show dropdown options for 'profile' and 'log out'
+        // If not logged in, show options for 'register' and 'log in'
+        const menuButtons = this.props.isLogged ? (
+            <div className="header-user-menu">
+                <NavLink to='/profile' style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <MenuItem className="header-user-menu-item" onClick={this.handleMenuToggleClose}>PROFILE</MenuItem>
+                </NavLink>
+                <MenuItem className="header-user-menu-item" onClick={this.handleMenuToggleClose}>LOG OUT</MenuItem>
             </div>
         ) : (
-            <div>
-                <Link to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <Button className="header-button">
-                        Log In
-                    </Button>
-                </Link>
+            <div className="header-user-menu">
+                <NavLink to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <MenuItem className="header-user-menu-item" onClick={this.handleMenuToggleClose}>LOG IN</MenuItem>
+                </NavLink>
+                <NavLink to='/signup' style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <MenuItem className="header-user-menu-item" onClick={this.handleMenuToggleClose}>REGISTER</MenuItem>
+                </NavLink>
             </div>
         );
 
@@ -69,7 +79,53 @@ class Header extends Component {
                                 CoProManager
                             </Link>
                         </Typography>
-                        { headerButtons }
+                        { /* If logged in, show user and admin buttons respectively */ }
+                        { this.props.isLogged && (
+                            <div>
+                                <NavLink to='/contests' style={{ textDecoration: 'none', color: 'inherit' }}>
+                                <IconButton
+                                    aria-label="Contests"
+                                    color="inherit"
+                                >
+                                    <FitnessCenter/>
+                                </IconButton>
+                                </NavLink>
+                                {this.props.isAdmin &&
+                                <NavLink to='/users' style={{textDecoration: 'none', color: 'inherit'}}>
+                                    <IconButton
+                                        aria-label="User List"
+                                        color="inherit"
+                                    >
+                                        <Group/>
+                                    </IconButton>
+                                </NavLink>
+                                }
+                            </div>
+                            ) }
+                        <IconButton
+                            aria-owns={isMenuOpen ? 'menu-appbar' : null}
+                            aria-haspopup="true"
+                            onClick={this.handleMenuToggle}
+                            color="inherit"
+                        >
+                            <Person />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={this.state.anchor}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={isMenuOpen}
+                            onClose={this.handleMenuToggleClose}
+                        >
+                            { menuButtons }
+                        </Menu>
                     </Toolbar>
                 </AppBar>
             </div>
