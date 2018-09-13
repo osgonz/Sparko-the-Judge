c96551b5d30d64660b3070105545a3e67685ab32
gerardogalvez <galvezgerardo01@gmail.com>
2018-09-13 18:59:10 -0500

    Get MySQL configuration from dotenv

--- flask-api/app.py
+++ flask-api/app.py
@@ -17,9 +17,9 @@ parser = reqparse.RequestParser()
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'CoProManager'
+app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
 app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
 
 mysql.init_app(app)
