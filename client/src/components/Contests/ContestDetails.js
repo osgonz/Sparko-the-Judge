42a8881aabce409d5940cd4c4561b1247f62f0b7
Oscar González <osdagoso@hotmail.com>
2018-10-04 21:54:09 -0500

    Added placeholder Contest title

--- client/src/components/Contests/ContestDetails.js
+++ client/src/components/Contests/ContestDetails.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import '../../style/style.css';
 
-function ContestDetails(props) {
-    return(
-        <div>
-            <h1 className="error-title">Error 404</h1>
-            <Typography className="notification-description" variant="subheading" color="textSecondary">
-                <p className="error-text">{"The page you're looking for doesn't exist!"}</p>
-            </Typography>
-        </div>
-    );
+class ContestDetails extends Component {
+    render()
+    {
+        return (
+            <div>
+                <h1 className="contest-title">Mi primer concurso</h1>
+                <Typography className="notification-description" variant="subheading" color="textSecondary">
+                    <p className="error-text">{"The page you're looking for doesn't exist!"}</p>
+                </Typography>
+            </div>
+        );
+    }
 }
 
 export default ContestDetails;
