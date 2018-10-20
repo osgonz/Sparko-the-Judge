e31a06f2ce5f74fc51e5a3aab29b7c2b53f78dad
guillermoams <ams.guillermo@gmail.com>
2018-10-20 18:57:32 -0500

    Woops! I accidentally added my port to the app.py

--- flask-api/app.py
+++ flask-api/app.py
@@ -26,7 +26,7 @@ app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
 app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
 app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
 app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
-app.config['MYSQL_DATABASE_PORT'] = 3307
+#app.config['MYSQL_DATABASE_PORT'] = 3307
 
 
 # Session configurations
